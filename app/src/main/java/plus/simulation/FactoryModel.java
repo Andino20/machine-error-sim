1b2d40bcefce5d6dec33476780847e609516bf59
Andreas Schlager <andreas.schlager@stud.plus.ac.at>
2025-06-15 10:17:01 +0200

    Add time distribution with values from assignment

--- app/src/main/java/plus/simulation/FactoryModel.java
+++ app/src/main/java/plus/simulation/FactoryModel.java
@@ -1,8 +1,12 @@
 package plus.simulation;
 
+import desmoj.core.dist.ContDistExponential;
 import desmoj.core.simulator.Model;
 
 public class FactoryModel extends Model {
+    private ContDistExponential machineBreakdownTime;
+    private ContDistExponential repairTime;
+
     public FactoryModel(String s) {
         super(null, s, true, true);
     }
@@ -23,6 +27,19 @@ public class FactoryModel extends Model {
 
     @Override
     public void init() {
+        // The distribution mean is in minutes
+        machineBreakdownTime = new ContDistExponential(this, "Machine Breakdown Time Distribution", 8 * 60, true, true);
+        repairTime = new ContDistExponential(this, "Repair Time Distribution", 2 * 60, true, true);
 
+        machineBreakdownTime.setNonNegative(true);
+        repairTime.setNonNegative(true);
+    }
+
+    public double getMachineBreakdownTime() {
+        return machineBreakdownTime.sample();
+    }
+
+    public double getRepairTime() {
+        return repairTime.sample();
     }
 }
