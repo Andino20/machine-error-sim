65184cf0853a9bed954339120dae01e420fc1863
Andreas Schlager <andreas.schlager@stud.plus.ac.at>
2025-06-15 19:45:43 +0200

    Fix constructor call

--- app/src/main/java/plus/simulation/App.java
+++ app/src/main/java/plus/simulation/App.java
@@ -8,7 +8,7 @@ import java.util.concurrent.TimeUnit;
 
 public class App {
     public static void main(String[] args) {
-        Model model = new FactoryModel("Factory Model");
+        Model model = new FactoryModel("Factory Model", 4, 1);
         Experiment exp = new Experiment("Factory Experiment", "../reports");
 
         model.connectToExperiment(exp);
